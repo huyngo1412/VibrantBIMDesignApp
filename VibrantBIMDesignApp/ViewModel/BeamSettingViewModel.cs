4ee39c8d9e82e2aa05bbc206f2894c8e39c2489a
ynhi180604 <ynhi8450@gmail.com>
2025-03-09 16:24:47 +0700

    update

--- VibrantBIMDesignApp/ViewModel/BeamSettingViewModel.cs
+++ VibrantBIMDesignApp/ViewModel/BeamSettingViewModel.cs
@@ -13,8 +13,6 @@ using VibrantBIMDesignApp.View.WindowForm;
 
 namespace VibrantBIMDesignApp.ViewModel
 {
-    
-
     public class BeamSettingViewModel : ViewModelBase
     {
         public ICommand LoadWindowCommand { get; set; }
@@ -23,25 +21,39 @@ namespace VibrantBIMDesignApp.ViewModel
             LoadWindowCommand = new  RelayCommand<Canvas>((p) => true, (p) =>
             {
                 var canvas = p as Canvas;
-                if (canvas != null)
-                {
-                    MessageBox.Show($"... {canvas.Name}");
-                }
+                if (canvas == null) return;
                 DrawCanvasAnnotation(canvas);
             });
-
         }
         private void DrawCanvasAnnotation(Canvas canvas)
         {
+            double CoverRebar = 15;
+            double DiameterRebar = 7.5;
+            double Spacing = ((100 - CoverRebar) / 2) - DiameterRebar/2;
+
             Rectangle section = new Rectangle();
             section.Width = 100;
             section.Height = 150;
             section.Fill = DrawAnnotation.Concrete;
             section.Stroke = DrawAnnotation.Concrete;
-            section.StrokeThickness = 10;
             Canvas.SetLeft(section, 50);
             Canvas.SetTop(section, 60);
             canvas.Children.Add(section);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Ellipse TopRebar = new Ellipse();
+                TopRebar.Width = DiameterRebar;
+                TopRebar.Height = DiameterRebar;
+                TopRebar.Fill = DrawAnnotation.Rebar;
+                TopRebar.Fill = DrawAnnotation.Rebar;
+                TopRebar.Stroke = DrawAnnotation.Rebar;
+                Canvas.SetLeft(TopRebar, 50  + Spacing*i);
+                Canvas.SetTop(TopRebar, 60 + CoverRebar - DiameterRebar/2);
+                canvas.Children.Add(TopRebar);
+
+
+            }
         }
     }
 }
--- VibrantBIMDesignApp/obj/Debug/net8.0-windows/View/UCView/ControlBarUC.g.i.cs
+++ VibrantBIMDesignApp/obj/Debug/net8.0-windows/View/UCView/ControlBarUC.g.i.cs
@@ -69,7 +69,7 @@ namespace VibrantBIMDesignApp.View.UCView {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -85,7 +85,7 @@ namespace VibrantBIMDesignApp.View.UCView {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.2.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
